916f131cb01733c78da70fd100d112ca05bbc883
Geoff Schultz <schultz.robert.g@gmail.com>
2017-12-02 09:15:34 -0800

    Finish exercise.

--- Day One/day_one.java
+++ Day One/day_one.java
@@ -7,16 +7,35 @@ import java.io.File;
 
 class day_one {
     public static void main(String[] args) throws IOException{
-        int first = 0;
-        int previous = 0;
-
+     
+        //Read input file, save as String "input_text"
         InputStream is = new FileInputStream(new File(args[0]));
         InputStreamReader input_stream = new InputStreamReader(is);
         BufferedReader reader = new BufferedReader(input_stream);
         String input_text = reader.readLine();
         reader.close();
 
-        System.out.println(input_text);
+        //total - final answer
+        int total = 0;
+
+        //j - index counter to follow i in the for loop (i-1)
+        int j = 0;
+
+        for(int i = 1; i < input_text.length(); i++){
+            j = i-1;
+
+            //if the two characters (i and i-1) are equal, take the int value of one and add to total
+            if(input_text.charAt(i) == input_text.charAt(j)){
+                total += Character.getNumericValue(input_text.charAt(i));
+            }
+        }
+
+        //compare first and last character
+        if(input_text.charAt(0) == input_text.charAt(input_text.length()-1)){
+            total += Character.getNumericValue(input_text.charAt(0));
+        }
+
+        System.out.println(total);
 
     }
 }
